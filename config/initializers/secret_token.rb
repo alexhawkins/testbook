64babe9b1031b8c9ed269de317ababb0236749f0
Rock Hawkins <rockhawkins.cc@gmail.com>
2014-01-27 14:56:54 -0800

    Created a rails appy and added some friggen status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testbook::Application.config.secret_key_base = '1d730a4277c664c620e51bf0693580bb5b919fb32a43ad71d1dd3641c5814fe3be350a3cb81d9bdc8f117c6205c23cbdfd8d73cde63a7cc0a7cdbe936e19caae'
